a561baf090ed8cd872c77b06639d96b4025d79d4
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-08-08 09:16:48 +0200

    Integration logo & Navlink

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,27 +1,20 @@
 import React from "react";
-import { BrowserRouter as Link } from "react-router-dom";
-import {logoKasa} from "../../assets/logo.svg"
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import logoKasa from "../../assets/logo.svg";
 
 function Header() {
   return (
-      <header className="header">
+    <Router>
+     <header className="header">
       <div className="logo">
          <img src={logoKasa} alt="logo"></img>
       </div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
+        <nav className="navbar">
+              <Link className="navHome" to="/">Accueil</Link>
+              <Link className="navAbout" to="/about">A propos</Link>
         </nav>
         </header>
+    </Router>
   );
 }
 
