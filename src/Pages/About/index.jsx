d8d5ddfc459dfa1e3eb11d654c01d2a86ef7770b
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-08-11 10:44:53 +0200

    Menu accordion sans CSS

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -1,7 +1,17 @@
-import { Link } from "react-router-dom";
+import React from 'react';
+import Accordion from '../../components/Accordion/index';
+import { accordionData } from '../../data/accordionData';
 
-function About() {
-
-}
+function About () {
+    return (
+        <div>
+            <div className='accordion'>
+                {accordionData.map(({title, content}) => (
+                    <Accordion title={title} content={content}/>
+                ))}
+            </div>
+        </div>
+    )
+};
  
 export default About;
--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -7,6 +7,7 @@ const Accordion = ({ title, content }) => {
         <div className="accordion-item">
             <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
                 <div>{title}</div>
+                <div>{isActive ? '-' : '+'}</div>
             </div>
             <div>
                 {isActive && <div className="accordion-content">{content}</div>}
