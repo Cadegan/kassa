661806aa5f219c5a728a6feb5eebe40a2a4b2901
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-08-13 13:03:18 +0200

    Card v0

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,13 @@
+function Card (props) {
+    const { id, title, picture } = props;
+    
+    return (
+        <article className="card" key={id}>
+            <p className="card-title"></p>
+            <img className="card-picture" scr={picture} alt={`${title}`}/>
+        </article>
+        
+    )
+}
+
+export default Card;
