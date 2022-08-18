1e1fa116680c81a803e2cd6a65bf9a9a9beb8565
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-08-18 21:31:26 +0200

    Data fetch House v0

--- src/Pages/House/index.jsx
+++ src/Pages/House/index.jsx
@@ -1,9 +1,96 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function House() {
+  const { id } = useParams;
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
-    const getData = () => {
-      fetch("/");
-    };
-  });
+    async function getData() {
+      try {
+        const response = await fetch("/logements.json", {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error(` - Error status ${response.status}`);
+        }
+        let houseData = await response.json();
+        let houseSelected = houseData.find((e) => e.id === id);
+        if (houseSelected) {
+          setData(houseSelected);
+          setError(null);
+        }
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getData();
+  }, [data, id]);
 }
+
+// export default House;
+
+// function Home() {
+//   const [data, setData] = useState(null);
+//   const [isLoading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   //   v2
+//   useEffect(() => {
+//     async function getData() {
+//       try {
+//         const response = await fetch("/logements.json", {
+//           headers: {
+//             "Content-Type": "application/json",
+//             Accept: "application/json",
+//           },
+//         });
+
+//         if (!response.ok) {
+//           throw new Error(` - Error status ${response.status}`);
+//         }
+//         let actualData = await response.json();
+//         setData(actualData);
+//         setError(null);
+//       } catch (err) {
+//         setError(err.message);
+//         setData(null);
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+//     getData();
+//   }, []);
+
+//   return (
+//     <section className="homeSection">
+//       <div className="homeBanner">
+//         <img src={imgBanner} alt="Bannière entête"></img>
+//         <p>Chez vous, partout et ailleurs</p>
+//       </div>
+//       <div className="homeCardSection">
+//         {isLoading && <Loader />}
+//         {error && (
+//           <div className="errorMessage">
+//             {`Erreur de chargement des données ${error}`}
+//           </div>
+//         )}
+//         <div className="cardsContainer">
+//           {data &&
+//             data.map(({ id, title, cover }) => (
+//               <Card id={id} key={id} title={title} cover={cover}></Card>
+//             ))}
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
